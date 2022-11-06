d08fe22cb0e8177275f5f8aafac42e83d9c396e8
benquang <thangfacebook1905@gmail.com>
2022-11-06 20:59:15 +0700

    commit to deve lan1

---
+++ src/main/java/murach/Controllers/Homework/Homework1.java
@@ -0,0 +1,56 @@
+package murach.Controllers.Homework;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import murach.Business.UserHomework;
+//import murach.data.UserDB;
+
+public class Homework1 extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+        String url = "/homework/1/index.jsp";
+        
+        // get current action
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "join";  // default action
+        }
+
+        // perform action and set URL to appropriate page
+        if (action.equals("join")) {
+            url = "/homework/1/index.jsp";    // the "join" page
+        } 
+        else if (action.equals("add")) {
+            // get parameters from the request
+            String firstName = request.getParameter("firstName");
+            String lastName = request.getParameter("lastName");
+            String email = request.getParameter("email");
+            
+            // store data in User object
+            UserHomework user = new UserHomework(firstName, lastName, email);
+
+            // validate the parameters
+            String message;
+            if (firstName == null || lastName == null || email == null ||
+                firstName.isEmpty() || lastName.isEmpty() || email.isEmpty()) {
+                message = "Please fill out all three text boxes.";
+                url = "/homework/1/index.jsp";
+            } 
+            else {
+                message = "";
+                url = "/homework/1/thanks.jsp";
+                //UserDB.insert(user);
+            }
+            request.setAttribute("user", user);
+            request.setAttribute("message", message);
+        }
+        getServletContext()
+                .getRequestDispatcher(url)
+                .forward(request, response);
+    }
+}
